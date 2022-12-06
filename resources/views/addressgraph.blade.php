d754816df314f9b74fbd2851f8296543923ab9d2
sandeeppal270 <sandeeppal270@gmail.com>
2022-12-06 10:41:18 +0530

    first commit

--- resources/views/addressgraph.blade.php
+++ resources/views/addressgraph.blade.php
@@ -150,13 +150,8 @@ chart.render();
 </script>
 <div id="chartContainer" style="height: 370px; width: 100%;"></div>
 <script src="https://canvasjs.com/assets/script/canvasjs.min.js"></script>
-</div>
-
-
-
-
-
-                    </div>
+        </div>
+                     </div>
                   </div>
                 </div>
          
